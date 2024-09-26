30077cb98187179a828b477b06e032310d33a987
Uofi113 <fdf64502@gmail.com>
2024-09-26 21:15:39 +0200

    Nitro Gen Not Stabil (1.2)

---
+++ Nitrustchka.py
@@ -0,0 +1,34 @@
+import requests
+import random
+import string
+import time
+
+def generate_nitro_code():
+    code = ""
+    for _ in range(16):
+        code += random.choice(string.ascii_letters + string.digits)
+    return code
+
+# юрл вебхука (Webhook Url)
+webhook_url = 'https://discord.com/api/webhooks/1288860285364342846/JT-CcAejyiVBkoKvi3LNp0R-JvmALgzAjXaw8Dhth0PwEwosVawS0Unp-FMoIFZypKqP'
+
+for _ in range(100):
+    nitro_code = generate_nitro_code()
+    nitro_url = "https://discord.gift/" + nitro_code
+
+    #Кастомка (не работает) (Custom message, dont work)
+
+    message = ""
+    
+    # Кидает сообщение (send message
+    data = {"content": nitro_url}
+    response = requests.post(webhook_url, json=data)
+    
+    if response.status_code == 204:
+        print("Отправил брат:", nitro_url)
+    else:
+        print("Не получилось отправить.")
+    
+    # Добавляет вэйт в 1 секунду, ибо дс ограничивает бота, если он отправляет очень много сообщений (1 second wait)
+    time.sleep(1)
+#maked by SamoreZ, or Uofi._
